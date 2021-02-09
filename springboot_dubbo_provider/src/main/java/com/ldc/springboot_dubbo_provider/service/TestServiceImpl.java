e6648eb34f4c2c4221f00c958661e63958a820d1
ldcaws <568702182@qq.com>
2021-02-09 23:10:18 +0800

    添加springboot+dubbo模块，提供者

---
+++ springboot_dubbo_provider/src/main/java/com/ldc/springboot_dubbo_provider/service/TestServiceImpl.java
@@ -0,0 +1,32 @@
+package com.ldc.springboot_dubbo_provider.service;
+
+import com.ldc.springboot_dubbo_interface.service.TestService;
+import org.apache.dubbo.config.annotation.DubboService;
+
+/**
+ * @description:
+ * @author: ldc
+ * @time: 2021/2/9 22:20
+ */
+@DubboService(version = "1.0.0",interfaceClass = TestService.class)
+public class TestServiceImpl implements TestService {
+    @Override
+    public void ins() {
+        System.out.println("insert");
+    }
+
+    @Override
+    public void del() {
+        System.out.println("delete");
+    }
+
+    @Override
+    public void upd() {
+        System.out.println("update");
+    }
+
+    @Override
+    public void sel() {
+        System.out.println("select");
+    }
+}
